7731f9140ca88f8207f456ba3e57e46ffe72e45d
Simge-157370 <80451730+Simge-157370@users.noreply.github.com>
2021-04-26 23:42:21 +0300

    Add files via upload

---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors/Rock_Paper_Scissors/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace Rock_Paper_Scissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Player_A = new System.Windows.Forms.PictureBox();
+            this.Player_B = new System.Windows.Forms.PictureBox();
+            this.Rock = new System.Windows.Forms.PictureBox();
+            this.Paper = new System.Windows.Forms.PictureBox();
+            this.Scissors = new System.Windows.Forms.PictureBox();
+            this.Play_Button = new System.Windows.Forms.Button();
+            this.PlayerALabel = new System.Windows.Forms.Label();
+            this.PlayerBLabel = new System.Windows.Forms.Label();
+            this.ScoreTableA = new System.Windows.Forms.Label();
+            this.ScoreTableB = new System.Windows.Forms.Label();
+            this.ScoreA = new System.Windows.Forms.Label();
+            this.ScoreB = new System.Windows.Forms.Label();
+            this.WinnerTable = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.quits_number = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Scissors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player_A
+            // 
+            this.Player_A.ErrorImage = null;
+            this.Player_A.Location = new System.Drawing.Point(30, 62);
+            this.Player_A.Name = "Player_A";
+            this.Player_A.Size = new System.Drawing.Size(220, 220);
+            this.Player_A.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player_A.TabIndex = 0;
+            this.Player_A.TabStop = false;
+            this.Player_A.Click += new System.EventHandler(this.Player_A_Click);
+            // 
+            // Player_B
+            // 
+            this.Player_B.Location = new System.Drawing.Point(662, 62);
+            this.Player_B.Name = "Player_B";
+            this.Player_B.Size = new System.Drawing.Size(220, 220);
+            this.Player_B.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player_B.TabIndex = 2;
+            this.Player_B.TabStop = false;
+            this.Player_B.Click += new System.EventHandler(this.Player_B_Click);
+            // 
+            // Rock
+            // 
+            this.Rock.Image = global::Rock_Paper_Scissors.Properties.Resources.rock_png;
+            this.Rock.Location = new System.Drawing.Point(30, 303);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(65, 65);
+            this.Rock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Rock.TabIndex = 3;
+            this.Rock.TabStop = false;
+            this.Rock.Click += new System.EventHandler(this.Rock_Click);
+            // 
+            // Paper
+            // 
+            this.Paper.Image = global::Rock_Paper_Scissors.Properties.Resources.paper_png;
+            this.Paper.Location = new System.Drawing.Point(101, 303);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(65, 65);
+            this.Paper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Paper.TabIndex = 5;
+            this.Paper.TabStop = false;
+            this.Paper.Click += new System.EventHandler(this.Paper_Click);
+            // 
+            // Scissors
+            // 
+            this.Scissors.Image = global::Rock_Paper_Scissors.Properties.Resources.scissors_png;
+            this.Scissors.Location = new System.Drawing.Point(172, 303);
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Size = new System.Drawing.Size(65, 65);
+            this.Scissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Scissors.TabIndex = 6;
+            this.Scissors.TabStop = false;
+            this.Scissors.Click += new System.EventHandler(this.Scissors_Click);
+            // 
+            // Play_Button
+            // 
+            this.Play_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Play_Button.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Play_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Play_Button.Location = new System.Drawing.Point(698, 308);
+            this.Play_Button.Name = "Play_Button";
+            this.Play_Button.Size = new System.Drawing.Size(150, 60);
+            this.Play_Button.TabIndex = 7;
+            this.Play_Button.Text = "PLAY";
+            this.Play_Button.UseVisualStyleBackColor = false;
+            this.Play_Button.Click += new System.EventHandler(this.Play_Button_Click);
+            // 
+            // PlayerALabel
+            // 
+            this.PlayerALabel.AutoSize = true;
+            this.PlayerALabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.PlayerALabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.PlayerALabel.Location = new System.Drawing.Point(55, 22);
+            this.PlayerALabel.Name = "PlayerALabel";
+            this.PlayerALabel.Size = new System.Drawing.Size(97, 29);
+            this.PlayerALabel.TabIndex = 8;
+            this.PlayerALabel.Text = "PlayerA";
+            // 
+            // PlayerBLabel
+            // 
+            this.PlayerBLabel.AutoSize = true;
+            this.PlayerBLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.PlayerBLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.PlayerBLabel.Location = new System.Drawing.Point(687, 22);
+            this.PlayerBLabel.Name = "PlayerBLabel";
+            this.PlayerBLabel.Size = new System.Drawing.Size(95, 29);
+            this.PlayerBLabel.TabIndex = 9;
+            this.PlayerBLabel.Text = "PlayerB";
+            // 
+            // ScoreTableA
+            // 
+            this.ScoreTableA.AutoSize = true;
+            this.ScoreTableA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ScoreTableA.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ScoreTableA.ForeColor = System.Drawing.Color.White;
+            this.ScoreTableA.Location = new System.Drawing.Point(271, 81);
+            this.ScoreTableA.Name = "ScoreTableA";
+            this.ScoreTableA.Size = new System.Drawing.Size(119, 29);
+            this.ScoreTableA.TabIndex = 10;
+            this.ScoreTableA.Text = "ScoreOfA";
+            // 
+            // ScoreTableB
+            // 
+            this.ScoreTableB.AutoSize = true;
+            this.ScoreTableB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ScoreTableB.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ScoreTableB.ForeColor = System.Drawing.Color.White;
+            this.ScoreTableB.Location = new System.Drawing.Point(515, 81);
+            this.ScoreTableB.Name = "ScoreTableB";
+            this.ScoreTableB.Size = new System.Drawing.Size(117, 29);
+            this.ScoreTableB.TabIndex = 11;
+            this.ScoreTableB.Text = "ScoreOfB";
+            // 
+            // ScoreA
+            // 
+            this.ScoreA.AutoSize = true;
+            this.ScoreA.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ScoreA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ScoreA.Location = new System.Drawing.Point(311, 140);
+            this.ScoreA.Name = "ScoreA";
+            this.ScoreA.Size = new System.Drawing.Size(27, 29);
+            this.ScoreA.TabIndex = 12;
+            this.ScoreA.Text = "0";
+            this.ScoreA.Click += new System.EventHandler(this.ScoreA_Click);
+            // 
+            // ScoreB
+            // 
+            this.ScoreB.AutoSize = true;
+            this.ScoreB.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ScoreB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ScoreB.Location = new System.Drawing.Point(554, 140);
+            this.ScoreB.Name = "ScoreB";
+            this.ScoreB.Size = new System.Drawing.Size(27, 29);
+            this.ScoreB.TabIndex = 13;
+            this.ScoreB.Text = "0";
+            // 
+            // WinnerTable
+            // 
+            this.WinnerTable.AutoSize = true;
+            this.WinnerTable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.WinnerTable.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.WinnerTable.ForeColor = System.Drawing.Color.Red;
+            this.WinnerTable.Location = new System.Drawing.Point(370, 232);
+            this.WinnerTable.MaximumSize = new System.Drawing.Size(170, 50);
+            this.WinnerTable.MinimumSize = new System.Drawing.Size(170, 50);
+            this.WinnerTable.Name = "WinnerTable";
+            this.WinnerTable.Size = new System.Drawing.Size(170, 50);
+            this.WinnerTable.TabIndex = 14;
+            this.WinnerTable.Text = "WİNNER";
+            this.WinnerTable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.WinnerTable.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(370, 282);
+            this.label2.MaximumSize = new System.Drawing.Size(170, 50);
+            this.label2.MinimumSize = new System.Drawing.Size(170, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 50);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Quits";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // quits_number
+            // 
+            this.quits_number.AutoSize = true;
+            this.quits_number.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.quits_number.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.quits_number.Location = new System.Drawing.Point(442, 418);
+            this.quits_number.Name = "quits_number";
+            this.quits_number.Size = new System.Drawing.Size(27, 29);
+            this.quits_number.TabIndex = 17;
+            this.quits_number.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(380, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 29);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "quitsNumber";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 508);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quits_number);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WinnerTable);
+            this.Controls.Add(this.ScoreB);
+            this.Controls.Add(this.ScoreA);
+            this.Controls.Add(this.ScoreTableB);
+            this.Controls.Add(this.ScoreTableA);
+            this.Controls.Add(this.PlayerBLabel);
+            this.Controls.Add(this.PlayerALabel);
+            this.Controls.Add(this.Play_Button);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.Rock);
+            this.Controls.Add(this.Player_B);
+            this.Controls.Add(this.Player_A);
+            this.Name = "Form1";
+            this.Text = "Score_Of_A";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Scissors)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player_A;
+        private System.Windows.Forms.PictureBox Player_B;
+        private System.Windows.Forms.PictureBox Rock;
+        private System.Windows.Forms.PictureBox Paper;
+        private System.Windows.Forms.PictureBox Scissors;
+        private System.Windows.Forms.Button Play_Button;
+        private System.Windows.Forms.Label PlayerALabel;
+        private System.Windows.Forms.Label PlayerBLabel;
+        private System.Windows.Forms.Label ScoreTableA;
+        private System.Windows.Forms.Label ScoreTableB;
+        private System.Windows.Forms.Label ScoreA;
+        private System.Windows.Forms.Label ScoreB;
+        private System.Windows.Forms.Label WinnerTable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label quits_number;
+    }
+}
+
---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors/Rock_Paper_Scissors/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string playerA_choice;
+        string playerB_choice;
+        int playerA_score;
+        int playerB_score;
+        int random_value;
+        int quits_value;
+        Random random_number = new Random();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Score();
+        }
+
+        private void Rock_Click(object sender, EventArgs e)
+        {
+            Player_A.Image = Properties.Resources.rock_png; 
+            playerA_choice = "rock";
+        }
+
+        private void Paper_Click(object sender, EventArgs e)
+        {
+            Player_A.Image = Properties.Resources.paper_png;
+            playerA_choice = "paper";
+        }
+
+        private void Scissors_Click(object sender, EventArgs e)
+        {
+            Player_A.Image = Properties.Resources.scissors_png;
+            playerA_choice = "scissors";
+        }
+
+        private void Player_A_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void PlayerBChoice()
+        {
+            random_value = random_number.Next(1, 4);
+            if (random_value == 1)
+            {
+                Player_B.Image = Properties.Resources.rock_png;
+                playerB_choice = "rock";
+            }
+            else if (random_value == 2)
+            {
+                Player_B.Image = Properties.Resources.paper_png;
+                playerB_choice = "paper";
+
+
+            }
+            else if (random_value == 3)
+            {
+                Player_B.Image = Properties.Resources.scissors_png;
+                playerB_choice = "scissors";
+            }
+        }
+
+        private void Play_Button_Click(object sender, EventArgs e)
+        {
+            if (playerA_score == 0 && playerB_score == 0)
+            {
+                label2.Visible = false;
+                WinnerTable.Visible = false;
+                ScoreA.Text = "0";
+                ScoreB.Text = "0";
+                quits_number.Text = "0";
+
+            }
+            if (playerA_choice == null)
+            {
+                MessageBox.Show("Please select a move", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                PlayerBChoice();
+            }
+            Score();
+        }
+        private void Score()
+        {
+            if (playerA_choice == "rock" && playerB_choice == "paper")
+            {
+                playerB_score++;
+                ScoreB.Text = playerB_score.ToString();
+
+            }
+            if (playerA_choice == "rock" && playerB_choice == "scissors")
+            {
+                playerA_score++;
+                ScoreA.Text = playerA_score.ToString();
+
+            }
+            if (playerA_choice == "rock" && playerB_choice == "paper")
+            {
+                playerB_score++;
+                ScoreB.Text = playerB_score.ToString();
+
+            }
+            if (playerA_choice == "paper" && playerB_choice == "rock")
+            {
+                playerA_score++;
+                ScoreA.Text = playerA_score.ToString();
+
+            }
+            if (playerA_choice == "paper" && playerB_choice == "scissors")
+            {
+                playerB_score++;
+                ScoreB.Text = playerB_score.ToString();
+
+            }
+            if (playerA_choice == "scissors" && playerB_choice == "rock")
+            {
+                playerB_score++;
+                ScoreB.Text = playerB_score.ToString();
+
+            }
+            if (playerA_choice == "scissors" && playerB_choice == "paper")
+            {
+                playerA_score++;
+                ScoreA.Text = playerA_score.ToString();
+
+            }
+            if (playerA_choice == "rock" && playerB_choice == "rock")
+            {
+                quits_value++;
+                quits_number.Text = quits_value.ToString();
+
+            }
+            if (playerA_choice == "paper" && playerB_choice == "paper")
+            {
+                quits_value++;
+                quits_number.Text = quits_value.ToString();
+
+            }
+            if (playerA_choice == "scissors" && playerB_choice == "scissors")
+            {
+                quits_value++;
+                quits_number.Text = quits_value.ToString();
+
+            }
+            if (playerA_score + playerB_score + quits_value == 100)
+            {
+                label2.Visible = true;
+                WinnerTable.Visible = true;
+                if (playerA_score > playerB_score)
+                {
+                    label2.Text = "Player A wins" + playerA_score + "of 100 games Player B wins" + playerB_score + "of 100 games" + "Tie:" + quits_number + "of 100 games";
+                    playerA_score = 0;
+                    playerB_score = 0;
+                   
+                    Player_A.Image = null;
+                    Player_B.Image = null;
+                    playerA_choice = null;
+                    playerB_choice = null;
+                }
+                else if (playerB_score > playerA_score)
+                {
+                    label2.Text = "Player B wins" + playerB_score + "of 100 games" + " Player A wins" + playerA_score + "of 100 games" + "Tie:" + quits_number + "of 100 games"; ;
+                    playerA_score = 0;
+                    playerB_score = 0;
+                  
+                    Player_A.Image = null;
+                    Player_B.Image = null;
+                    playerA_choice = null;
+                    playerB_choice = null;
+
+
+                }
+                else if (playerB_score == playerA_score)
+                {
+                  
+                    label2.Text = "Quits";
+                 
+                    playerA_score = 0;
+                    playerB_score = 0;
+
+                    Player_A.Image= null;
+                    Player_B.Image = null;
+                    playerA_choice = null;
+                    playerB_choice = null;
+
+
+                }
+            }
+
+        }
+
+        private void Player_B_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Result_Table_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ScoreA_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
